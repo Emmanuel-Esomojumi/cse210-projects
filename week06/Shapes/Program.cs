bb1bf280da7f4dcc1314f472400d26f83bd2495a
Esomojumi-Emmanuel <eesomojumi@byupathway.edu>
2026-06-12 21:39:58 +0100

    complete Week 06 polymorphism Shapes project

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create a list of Shape objects
+        List<Shape> shapes = new List<Shape>();
+
+        // Add different kinds of shapes to the same list
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        // Demonstrate polymorphism
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
